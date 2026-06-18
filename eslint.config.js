619936fefd36cac0952c987ef85502ff6cbea2ee
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2026-06-17 19:35:17 -0700

    Set up project with ESLint and Prettier configuration

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  eslintConfigPrettier,
+);
